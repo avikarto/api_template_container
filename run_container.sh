d26022d0752be745b708c3ad953e9081da92e785
Andrew Vikartofsky <avikarto@gmail.com>
2022-08-03 16:45:42 -0400

    cleanup old containers on run

--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+docker stop api_demo || true
+docker container rm api_demo || true
 docker run -itd\
-    --mount type=bind,source="~/Documents/api_template_container/src",target=/src\
+    --mount type=bind,source="$HOME/Documents/api_template_container/src",target=/src\
     --name "api_demo"\
     -p 16001:16001\
     api_demo:latest
